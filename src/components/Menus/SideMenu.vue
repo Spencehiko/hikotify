119321a1a0bc266b59a5ad339843c519f5aa318f
Hikmet Can <hiko.kufte@gmail.com>
2022-05-07 23:46:14 +0300

    fix! little css fix

--- src/components/Menus/SideMenu.vue
+++ src/components/Menus/SideMenu.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup></script>
 <template>
-    <div class="w-48 h-screen bg-black p-5">
+    <div class="w-48 h-screen bg-black p-2">
         <img src="hikotify-wide.png" class="mx-auto w-full mb-5" alt="Logo" />
-        <div class="routers">
-            <div class="home w-full">
+        <div class="flex flex-col justify-center items-center p-2">
+            <div class="home w-full flex flex-row cursor-pointer">
                 <button class="flex justify-start w-full">
-                    <div class="home-logo inline-block"></div>
+                    <div
+                        class="home-logo inline-block w-8 h-8 mr-3 bg-white"
+                    ></div>
                     <span class="font-semibold my-auto text-2xl">Home</span>
                 </button>
             </div>
@@ -13,22 +15,8 @@
     </div>
 </template>
 <style lang="less" scoped>
-.routers {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    .home {
-        display: flex;
-        flex-direction: row;
-        &-logo {
-            width: 30px;
-            height: 30px;
-            background-color: #fff;
-            mask: url(home.svg) no-repeat center;
-            -webkit-mask: url(home.svg) 0 0/30px 30px no-repeat;
-            margin-right: 10px;
-        }
-    }
+.home-logo {
+    mask: url(home.svg) no-repeat center;
+    -webkit-mask: url(home.svg) 0 0/32px 32px no-repeat;
 }
 </style>
